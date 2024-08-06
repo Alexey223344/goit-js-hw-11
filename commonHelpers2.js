6ff37a0c46462c5f63185481fdd3066e6a5a79ec
Alexey223344 <160915208+Alexey223344@users.noreply.github.com>
2024-08-06 12:03:41 +0000

    Deploying to gh-pages from @ Alexey223344/goit-js-hw-11@8713be6dcacd06452489b986d03f638f3c119238 🚀

--- commonHelpers2.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i}from"./assets/vendor-BbSUbo7J.js";function o(e,r){return new Promise((s,t)=>{setTimeout(()=>{r?s(`✅ Fulfilled promise in ${e}ms`):t(`❌ Rejected promise in ${e}ms`)},e)})}document.querySelector("form").addEventListener("submit",e=>{e.preventDefault();const r=parseInt(e.target.delay.value),s=e.target.state.value==="fulfilled";o(r,s).then(t=>{i.success({message:t})}).catch(t=>{i.error({message:t})})});
-//# sourceMappingURL=commonHelpers2.js.map
